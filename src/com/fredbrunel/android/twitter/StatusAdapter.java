763b8dcd71434c550ae7dafdd8f20eded82e0ba7
Fred Brunel <fbrunel@gmail.com>
2007-11-19 05:11:05 +0000

    CHANGE: Move the status view code into the StatusAdapter.

--- src/com/fredbrunel/android/twitter/StatusAdapter.java
+++ src/com/fredbrunel/android/twitter/StatusAdapter.java
@@ -1,15 +1,24 @@
 package com.fredbrunel.android.twitter;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import android.content.Context;
 import android.database.ContentObserver;
 import android.database.DataSetObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout.LayoutParams;
 
+import jtwitter.TwitterEntry;
 import jtwitter.TwitterResponse;
 
 public class StatusAdapter implements ListAdapter {
@@ -23,7 +32,7 @@ public class StatusAdapter implements ListAdapter {
 		
 		// Create all views
 		for (int i = 0; i < statuses.getNumberOfItems(); i++) {
-			views.put(i, StatusView.makeView(context, statuses.getItemAt(i)));
+			views.put(i, makeView(context, statuses.getItemAt(i)));
 		}
 	}
 	
@@ -70,4 +79,31 @@ public class StatusAdapter implements ListAdapter {
 	
 	public void unregisterDataSetObserver(DataSetObserver observer) {
 	}
+	
+	// Create the view for each user status
+	// [FIXME] Should be put as an XML file
+	
+    private View makeView(Context context, TwitterEntry entry) 
+		throws IOException {
+
+    	ImageView iv = new ImageView(context);
+    	Bitmap photo = BitmapFactory.decodeStream(entry.getUser().getProfileImageURL().openStream());
+	
+    	iv.setImageBitmap(photo);
+    	iv.setScaleType(ScaleType.CENTER);
+    	iv.setPadding(0, 4, 4, 0);
+    	iv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	
+    	TextView tv = new TextView(context);
+    	tv.setText(entry.getUser().getName() + ": " + entry.getText());
+    	tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	
+    	LinearLayout layout = new LinearLayout(context);
+    	layout.setOrientation(LinearLayout.HORIZONTAL);
+    	layout.setPadding(4, 2, 4, 2);
+    	layout.addView(iv);
+    	layout.addView(tv);
+	
+    	return layout;
+    }
 }
