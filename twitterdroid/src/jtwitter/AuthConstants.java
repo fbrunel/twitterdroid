4540c8ca157cd993aafcd9c4888140538fdd2c1d
Nick Plante <nap@zerosum.org>
2010-12-20 15:15:49 -0500

    add note about obtaining auth credentials from Twitter

--- twitterdroid/src/jtwitter/AuthConstants.java
+++ twitterdroid/src/jtwitter/AuthConstants.java
@@ -4,8 +4,8 @@ import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthProvider;
 
 public interface AuthConstants {
-	public static final String CONSUMER_KEY = "iUwjRSp5vzqSFMWfbNysA";
-	public static final String CONSUMER_SECRET = "a52EB1b1H18pblgKgKWIRhDJli1zE2vN60hNsDIc";
+	public static final String CONSUMER_KEY = "YOUR_CONSUMER_KEY";
+	public static final String CONSUMER_SECRET = "YOUR_CONSUMER_SECRET";
 
 	public static final String REQUEST_URL = "http://twitter.com/oauth/request_token";
 	public static final String ACCESS_TOKEN_URL = "http://twitter.com/oauth/access_token";
